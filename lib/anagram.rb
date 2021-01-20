2c678f88a6d7b0ed6bad0cddb3a4805d823a87f2
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-01-19 16:41:59 -0800

    commit tree 93adace1b49a73e76b45c869ca1e054d7c600f38

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+    def initialize(word)
+    @word = word
+    end
+
+    attr_accessor :word 
+    def match(array)
+        letters = "#{@word}".chars.sort
+        @anagram_array = []
+        array.each do|anagram|
+            if anagram.chars.sort == letters
+            @anagram_array << "#{anagram}" 
+            end
+        end
+        @anagram_array
+    end 
+end
+
